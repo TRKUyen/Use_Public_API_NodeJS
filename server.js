46e17194e59d5a55857ad797c25c88c945028c51
TRKUyen <kimuyen425@gmail.com>
2024-08-02 18:26:54 +0700

    Fix the load render index.ejs and load data from server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 const port = 3000;
 
 // Serve the public folder as static files
-app.use(express.static("public"));
-
+app.set("views", "./views"); // assuming your views directory is inside src
+app.set("view engine", "ejs");
 // Render the index template with default values for weather and error
 app.get("/", (req, res) => {
   res.render("pages/index.ejs", { weather: null, error: null });
@@ -17,26 +17,31 @@ app.get("/", (req, res) => {
 app.get("/weather", async (req, res) => {
   // Get the city from the query parameters
   const city = req.query.city;
+  const API_URL = "https://api.openweathermap.org/data/2.5/";
   const apiKey = "1cdfff4b6f9125708b6f2ddef149d19c"; //pass your APIkey here
 
+  const config = {
+    params: {
+      q: city,
+      appid: apiKey,
+      units: "metric", // optional, but recommended to get temperature in Celsius
+    },
+  };
+
+  let weather = null;
+  let error = null;
   // Add your logic here to fetch weather data from the API
-  const API_URL = "https://api.openweathermap.org/data/2.5/";
   try {
-    const response = await axios.get(
-      API_URL + `weather?q=${city}&appid=${apiKey}`,
-      {
-        params: {
-          APIKey: apiKey,
-        },
-      }
-    );
-    const weather = response.data;
+    const response = await axios.get(API_URL + "weather", config);
+    weather = response.data;
+    console.log(weather);
     // Render the index template with the weather data and error message
-    res.render("./views/pages/index.ejs", { weather, error });
-  } catch (error) {
-    console.error("Failed to make request:", error.message);
+    res.render("pages/index.ejs", { weather: weather, error: null });
+  } catch (err) {
+    error = "Error fetching weather data. Please try again later.";
     res.render("pages/index.ejs", {
-      error: error.message,
+      weather: null,
+      error: error,
     });
   }
 });
